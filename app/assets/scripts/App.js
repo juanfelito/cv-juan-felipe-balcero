bfe264670e0c81cc41a2a04df133ed8a52cdc6ba
juanfelito <felipe.balcero45@com>
2018-09-06 15:04:28 -0500

    Modal complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
